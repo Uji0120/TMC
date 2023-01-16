0e27092d0ef35fd2bdeccd67961fbedd2c32d8f4
Yuji Iizuka <110681989+Uji0120@users.noreply.github.com>
2023-01-16 14:16:25 +0000

    1/16，2回目の更新

--- app/controllers/user/posts_controller.rb
+++ app/controllers/user/posts_controller.rb
@@ -1,5 +1,16 @@
 class User::PostsController < ApplicationController
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to post_path(@post.id), {success: "投稿しました。"}
+    else
+
+    end
   end
 
   def index
--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,10 +1,37 @@
 class User::UsersController < ApplicationController
   def show
+    @user = current_user
   end
 
   def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      flash[:success] = "更新に成功しました。"
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
   end
 
   def unsubscribe
   end
+
+  def withdrawal
+    @user = User.find(params[:id])
+    @user.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:las_name, :first_name, :last_name_kana, :first_name_kana,
+    :introduction, :is_deleted)
+  end
+
 end
