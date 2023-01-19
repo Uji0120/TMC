15183de5efb90673e5d36e9a24b0bcf99d936050
EC2 Default User <ec2-user@ip-172-31-2-119.ap-northeast-1.compute.internal>
2023-01-19 13:03:19 +0000

    1/19更新

---
+++ app/controllers/user/registrations_controller.rb
@@ -0,0 +1,13 @@
+class User::RegistrationsController < Devise::RegistrationsController
+    before_action :configure_sign_up_params, only: [:create]
+    
+    def after_sign_up_path_for(resource)
+        user_path(current_user)
+    end
+    
+    def configure_sign_up_params
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :last_name_kana, :first_name_kana, :introduction,])
+    end
+
+
+end
