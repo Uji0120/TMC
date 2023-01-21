94feaf738cf06d817aca2c6a6432355d2d8d31e3
EC2 Default User <ec2-user@ip-172-31-2-119.ap-northeast-1.compute.internal>
2023-01-21 12:04:09 +0000

    1/21更新

--- app/controllers/user/posts_controller.rb
+++ app/controllers/user/posts_controller.rb
@@ -9,16 +9,50 @@ class User::PostsController < ApplicationController
     if @post.save
       redirect_to post_path(@post.id), {success: "投稿しました。"}
     else
-
+      @posts = Post.all
+      @user = current_user
+      render :index
     end
   end
 
   def index
+    @posts = Post.all
+    @user = current_user
   end
 
   def show
+    @post = Post.find(params[:id])
+    @user = @post.user
   end
 
   def edit
+    @post = Post.find(params[:id])
+    if @post.user_id != current_user.id
+      redirect_to posts_path
+    end
   end
+  
+  def update
+    @post = Post.find(params[:id])
+    if @Post.update(post_params)
+      redirect_to post_path(@post.id), flash: {success: "投稿内容を更新しました。" }
+    else
+      #@book = Book.find(params[:id])
+      render :edit
+    end
+  end
+  
+  def destroy
+    @posts = Post.find(params[:id])
+    @posts.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :introdu, :image)
+  end
+
+  
 end
--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -6,6 +6,14 @@ class Users::SessionsController < Devise::SessionsController
     redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
   end
   
+  proteced
   
+  def user_state
+    @user = User.find_by(email: params[:user][:email])
+    return if !@user
+    if @user.vaild_password?(params[:user][:password]) && (@user.is_deleted)
+      redirect_to new_user_session_path
+    end
+  end
   
 end
